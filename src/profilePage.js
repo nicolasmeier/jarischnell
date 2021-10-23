13283ff261ecd1411eba414a0a165070309eb18f
nguyesyd <nguyesyd@students.zhaw.ch>
2021-10-23 10:38:03 +0200

    Implement first Profile function

--- src/profilePage.js
+++ src/profilePage.js
@@ -1,5 +1,11 @@
 export default function Profile() {
     return (
-      <div>My Stato Profile!</div>
+      <div>My Stato Profile
+          <h1>jarischnell</h1>
+          <a href="https://cdn.icon-icons.com/"><img src="https://cdn.icon-icons.com/icons2/1465/PNG/512/303familymanwomanboyboy1_100514.png"></img></a>
+
+      </div>
+      
+
     )
   }
