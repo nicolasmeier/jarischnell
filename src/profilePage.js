decb03facb2a1a90dc62c700dd5223c6ed3f67bd
nguyesyd <nguyesyd@students.zhaw.ch>
2021-10-23 12:23:29 +0200

    Add Batches

--- src/profilePage.js
+++ src/profilePage.js
@@ -1,10 +1,41 @@
-export default function Profile() {
+  export default function Profile() {
     return (
-      <div>My Stato Profile
-          <h1>jarischnell</h1>
-          <a href="https://cdn.icon-icons.com/"><img src="https://cdn.icon-icons.com/icons2/1465/PNG/512/303familymanwomanboyboy1_100514.png" alt="profile-picture" boarder="1"></img></a>
-      </div>
-      
+      <div className="container mx-auto">
+        <div className="md:flex bg-gray-200 rounded-xl p-8 md:p-0">
+        <img
+            className="w-32 h-32 my-6 md:w-80 md:h-auto md:rounded-none rounded-full mx-64"
+            src="https://cdn.icon-icons.com/icons2/1465/PNG/512/303familymanwomanboyboy1_100514.png"
+            alt=""
+            width="884"
+            height="512"
+          />
+          <div className="pt-0 md:p-0 text-left my-6 ">
+            <blockquote>
+              <p className="text-lg">
+                <h3>Infos</h3>
+                    <p className="text-gray-400">_________________</p>
+                    <div className="text-gray-600">
+                        <li>Name <p className="text-blue-300">jarischnell </p></li>  
+                        <li>Wohnform <p className="text-blue-300">WG </p></li>
+                        <li>Zimmer <p className="text-blue-300">5.5 </p></li>
+                        <li>Anzahl Personen<p className="text-blue-300">4 </p></li>
+                        <li>Energieformen<p className="text-blue-300">Solar</p></li>
+                    </div>              
+              </p>
+            </blockquote>
+          </div>      
+        </div>
+        <div className="">
+            
+            <h3 className="text-left pt-6">Batches</h3>
+            <p className="text-gray-400">_________________</p>
+            <div className="text-3xl pt-6"> 
+                😜     👽     👾     🎃     🧜🏻‍♀️     🏄🏽     🚵🏽     🔥     🌪     🌈     🏃‍♀️    🐛 
 
-    )
+                    👨‍🎨     👨‍🌾    👩‍🍳     🙉    🐋 
+            </div>
+
+        </div>
+      </div>
+    );
   }
